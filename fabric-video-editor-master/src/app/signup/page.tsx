e533e40dcb3f8bf98cbb74226156c50399a34371
Junhan Li <junhanli1@outlook.com>
2025-04-08 16:32:53 +0200

    add Google SignUp at Signup Page

--- fabric-video-editor-master/src/app/signup/page.tsx
+++ fabric-video-editor-master/src/app/signup/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"; //Google OAuth
+import { auth } from "@/utils/firebaseConfig";
 
 export default function Signup() {
   const [name, setName] = useState("");
@@ -41,8 +43,25 @@ export default function Signup() {
     }
   };
 
-  const handleGoogleSignup = async () => {
+  const handleGoogleSignup = async (e: React.FormEvent) => {
     // TODO: Implement Google OAuth signup
+    e.preventDefault();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        console.log(user);
+        window.location.href = "/editor";
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(errorCode, errorMessage, email, credential);
+      });
     console.log("Signup with Google");
   };
 
